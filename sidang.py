0df4effda2d20d1ac2bdba9c3d9595cc699d04ea
DiahDSyntia <83937381+DiahDSyntia@users.noreply.github.com>
2024-05-18 14:11:35 +0700

    Update sidang.py

--- sidang.py
+++ sidang.py
@@ -146,7 +146,7 @@ if selected == "Pre-Processing":
 
 if selected == "Modelling":
     st.write("Hasil Akurasi, Presisi, Recall, F1- Score Metode SVM")
-    data = pd.read_csv('https://raw.githubusercontent.com/DiahDSyntia/hipertensitugas/main/datanorm.csv', sep=';')
+    data = pd.read_csv('https://raw.githubusercontent.com/DiahDSyntia/hipertensitugas/main/datanorm1.csv', sep=';')
 
     # Memisahkan fitur dan target
     X = data[['Usia', 'IMT', 'Sistole', 'Diastole', 'Nafas','Detak Nadi','JK_L','JK_P']]
@@ -238,7 +238,7 @@ if selected == "Modelling":
         st.markdown(html_code, unsafe_allow_html=True)
 
 if selected == "Implementation":
-    data = pd.read_csv('https://raw.githubusercontent.com/DiahDSyntia/hipertensitugas/main/datanorm.csv', sep=';')
+    data = pd.read_csv('https://raw.githubusercontent.com/DiahDSyntia/hipertensitugas/main/datanorm1.csv', sep=';')
         
     #st.write("Dataset Hipertensi : ", data)
     
@@ -334,11 +334,11 @@ if selected == "Implementation":
             'Detak Nadi': [Detak_nadi]
         }
         new_data = pd.DataFrame(data)
-        datatest = pd.read_csv('https://raw.githubusercontent.com/DiahDSyntia/hipertensitugas/main/X_testing1.csv')  
+        datatest = pd.read_csv('https://raw.githubusercontent.com/DiahDSyntia/hipertensitugas/main/X_test1.csv')  
         datatest = pd.concat([datatest, new_data], ignore_index=True)
         #st.write(datatest)
-        datanorm = joblib.load('scalermmodel.pkl').fit_transform(datatest)
-        datapredict = joblib.load('rbfmodelling.pkl').predict(datanorm)
+        datanorm = joblib.load('scalermmodel (1).pkl').fit_transform(datatest)
+        datapredict = joblib.load('rbfmodel.pkl').predict(datanorm)
 
         st.write('Data yang Diinput:')
         st.write(f'- Jenis Kelamin: {Jenis_Kelamin}, Usia: {Usia}, IMT: {IMT}, Sistole: {Sistole}, Diastole: {Diastole}, Nafas: {Nafas}, Detak Nadi: {Detak_nadi}')
